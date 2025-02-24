f4021402968f6a0970b84550eeae05bb308b5967
abelarcos <abel302025@hotmail.com>
2025-02-24 17:55:30 -0600

    agregando factoris de asociaciones y seeder

---
+++ database/factories/AsociacionFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Asociacion>
+ */
+class AsociacionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+
+            //nombres falsos
+            'nombre' => $this->faker->unique()->company, // Genera nombres de empresa únicos
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,8 @@ class DatabaseSeeder extends Seeder
             JuezSeeder::class,
             AcademiaSeeder::class,
             AfiliacionSeeder::class,
+            AsociacionSeeder::class,
+
         ]);
     }
 }
